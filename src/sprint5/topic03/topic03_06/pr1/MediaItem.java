82b8ca57f01937334ffa035c816923c743a6a95a
Office <modestpda@gmail.com>
2024-10-01 10:25:06 +0000

    new task movie serials

---
+++ src/sprint5/topic03/topic03_06/pr1/MediaItem.java
@@ -0,0 +1,9 @@
+package sprint5.topic03.topic03_06.pr1;
+public interface MediaItem {
+
+    // Длительность фильма или сериала в минутах
+    public int getRuntime();
+
+    public String getTitle();
+
+}
